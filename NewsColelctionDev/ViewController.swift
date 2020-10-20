67769163834fa936f74b90a9904a6992324f342f
Take111 <maxmaxastea@gmail.com>
2020-10-20 09:25:53 +0900

    Initial Commit

---
+++ NewsColelctionDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsColelctionDev
+//
+//  Created by 竹ノ内愛斗 on 2020/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
